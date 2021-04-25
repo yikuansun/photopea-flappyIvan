f413e729321e07158f6d6ee65844e3b56e1583ce
Yikuan Sun <yikuansun2015@gmail.com>
2021-04-25 17:15:41 -0400

    Move the ship higher up

--- code.js
+++ code.js
@@ -19,7 +19,7 @@ async function initGame() {
 
     var playerPos = [960, 1040];
     await Photopea.runScript(window.parent, "app.open('https://yikuansun.github.io/dumbspacething/img/player_ship.png', null, true);");
-    await Photopea.runScript(window.parent, `app.activeDocument.activeLayer.translate(${960 - 97}, ${1040 - 77})`);
+    await Photopea.runScript(window.parent, `app.activeDocument.activeLayer.translate(${960 - 97}, ${1000 - 77})`);
 }
 
 Photopea.runScript(window.parent, "app.documents.add(1920, 1080, 72, 'cool game')").then(async function() {
