13e30bb0543859ab527923e8f77766f9bf4ca02c
Yikuan Sun <yikuansun2015@gmail.com>
2021-04-25 17:03:07 -0400

    Make a ship

--- code.js
+++ code.js
@@ -6,6 +6,12 @@ window.addEventListener("focus", function(e) {
     document.querySelector("span").style.display = "none";
 });
 
+async function initGame() {
+    var playerPos = [960, 1040];
+    await Photopea.runScript(window.parent, "app.open('https://yikuansun.github.io/dumbspacething/img/player_ship.png', null, true);");
+    await Photopea.runScript(window.parent, `app.activeDocument.activeLayer.translate(${960 - 97}, ${1040 - 77})`);
+}
+
 Photopea.runScript(window.parent, "app.documents.add(1920, 1080, 72, 'cool game')").then(async function() {
     await Photopea.runScript(window.parent, "app.UI.fitTheArea()");
 });
