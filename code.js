37fb14c47c777deb967e669dad356e11b903bd1a
Yikuan Sun <yikuansun2015@gmail.com>
2021-04-25 17:12:44 -0400

    C'mon Github Pages wtf

--- code.js
+++ code.js
@@ -7,6 +7,16 @@ window.addEventListener("focus", function(e) {
 });
 
 async function initGame() {
+    var point_circle = function(point, circle) {
+        var x = point.x;
+        var y = point.y;
+        var h = circle.x;
+        var k = circle.y;
+        var r = circle.radius;
+        if (Math.pow(x - h, 2) + Math.pow(y - k, 2) <= Math.pow(r, 2)) return true;
+        else return false;
+    };
+
     var playerPos = [960, 1040];
     await Photopea.runScript(window.parent, "app.open('https://yikuansun.github.io/dumbspacething/img/player_ship.png', null, true);");
     await Photopea.runScript(window.parent, `app.activeDocument.activeLayer.translate(${960 - 97}, ${1040 - 77})`);
