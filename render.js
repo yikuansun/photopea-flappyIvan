a861008891b34fde6f678d1a37ff9171ffa4a1a8
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-06-07 16:28:13 -0400

    Oosp
    Oosp

--- render.js
+++ render.js
@@ -11,6 +11,6 @@ function setPosScript(layerName, x, y) {
 async function render() {
     await Photopea.runScript(window.parent, setPosScript("ivanHead", playerPos[0], playerPos[1]));
     for (var i = 0; i < obstaclesPos.length; i++) {
-        await Photopea.runScript(window.parent, setPosScript("dinoBill" + i, obstaclesPos[i].x, playerPos[i].y));
+        await Photopea.runScript(window.parent, setPosScript("dinoBill" + i, obstaclesPos[i].x, obstaclesPos[i].y));
     }
 }
