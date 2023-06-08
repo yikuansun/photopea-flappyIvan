62083d658402f6dd389cd07f0467b9c38a9f4dac
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-06-07 20:51:56 -0400

    Oosp
    Oosp

--- setup.js
+++ setup.js
@@ -35,11 +35,12 @@ async function setup() {
     };
     score = 0;
 
+    await Photopea.runScript(window.parent, "app.documents.add(800, 700, 72, 'Flappy Ivan Kutskir')");
+    await Photopea.runScript(window.parent, "app.UI.fitTheArea()");
+
     await addImageAndWait(window.parent, "https://yikuansun.github.io/photopea-flappyIvan/img/bgPlate.png");
     await Photopea.runScript(window.parent, `app.activeDocument.activeLayer.name = "bgPlate";`);
 
-    await Photopea.runScript(window.parent, "app.documents.add(800, 700, 72, 'Flappy Ivan Kutskir')");
-    await Photopea.runScript(window.parent, "app.UI.fitTheArea()");
     await addImageAndWait(window.parent, "https://yikuansun.github.io/photopea-flappyIvan/img/ivanHead.png");
     await Photopea.runScript(window.parent, `app.activeDocument.activeLayer.name = "ivanHead";`);
 
